246da7bbdc7359064ab2aa270aba6fea068f0e55
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2016-09-09 14:00:15 -0400

    Updated from Brython Server: 9/9/2016 2:00:15 PM

--- tutorial1.py
+++ tutorial1.py
@@ -10,10 +10,15 @@ black = Color(0x000000, 1.0)
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
 # A graphics asset that represents a rectangle
-rectangle = RectangleAsset(50, 20, thinline, blue)
+rectangleBlue = RectangleAsset(50, 50, thinline, blue)
+rectangleRed = RectangleAsset(50, 50, thinline, red)
+elipseBlue=EllipseAsset(25,40,thinline,blue)
+polygon=PolygonAsset([(
 
 # Now display a rectangle
-Sprite(rectangle, (200, 50))
+Sprite(rectangleBlue, (200, 50))
+Sprite(rectangleRed, (160, 40))
+Sprite(elipseBlue,(300,100))
 
 myapp = App()
 myapp.run()
